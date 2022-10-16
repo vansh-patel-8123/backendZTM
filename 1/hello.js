859e7be27b09dc79b56f8b3998b846b99173eb53
Vansh Patel <vanshpatel1299@gmail.com>
2022-10-16 23:47:41 +0530

    basic node

---
+++ 1/hello.js
@@ -0,0 +1,21 @@
+// 1 --------------------------------------------------------
+// const mission = "learn";
+
+// if(mission === "learn"){
+//     console.log("Time to write some Node code!");
+// } else{
+//     console.log(`Is ${mission} really more fun ?`);
+// }
+
+// 2 --------------------------------------------------------
+// const mission = process.argv[2];
+
+// if(mission === "learn"){
+//     console.log("Time to write some Node code!");
+// } else{
+//     console.log(`Is ${mission} really more fun ?`);
+// }
+
+// node hello.js learn    -> passed argument 
+
+
